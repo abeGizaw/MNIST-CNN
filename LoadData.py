e6f3f6be6a08c8d95af6df146886a88ee9663c18
abeGizaw <gizawabe3@gmail.com>
2024-04-13 13:34:45 -0400

    data properly formatted and abstracted

--- LoadData.py
+++ LoadData.py
@@ -18,3 +18,18 @@ def load_mnist_labels(filename):
 
         labels = np.frombuffer(f.read(), dtype=np.uint8)
         return labels
+
+def formatData(train_images, test_images, train_labels, test_labels):
+    # Normalize data
+    X_train = (np.array(train_images) > 0).astype(np.float32)
+    X_test = (np.array(test_images) > 0).astype(np.float32)
+
+    # Reshape to be a list of lists. Inner lists contain an output
+    # from 0-9 per each output neuron
+    train_labels.reshape(-1, 1)
+    test_labels.reshape(-1, 1)
+
+    # Flatten data before passing into network
+    X_train = X_train.reshape((-1, 784))  # Flatten the images to 1D array of 784 features
+    X_test = X_test.reshape((-1, 784))  # Image is 28X28
+    return X_train, X_test, train_labels, test_labels
--- Training.py
+++ Training.py
@@ -1,45 +1,33 @@
 import numpy as np
-from LoadData import load_mnist_images, load_mnist_labels
-from nnfs.datasets import spiral_data
+from LoadData import load_mnist_images, load_mnist_labels, formatData
 from FeedForward import (DenseLayer, ActivationReLU, LossCategoricalCrossEntropy,
                             ActivationSoftmax, OptimizerSGD,AccuracyCategorical)
 from Model import Model
 
 
-# Load the data
+# Load and format the data
 train_images = load_mnist_images('MNISTdata\\train-images-idx3-ubyte')
 train_labels = load_mnist_labels('MNISTdata\\train-labels-idx1-ubyte')
 test_images = load_mnist_images('MNISTdata\\t10k-images-idx3-ubyte')
 test_labels = load_mnist_labels('MNISTdata\\t10k-labels-idx1-ubyte')
+print(f'X_train shape: {train_images.shape}')
+print(f'y_train shape: {train_labels.shape}')
+print(f'X_test shape: {test_images.shape}')
+print(f'y_test shape: {test_labels.shape}')
+X_train, X_test, train_labels, test_labels = formatData(train_images, test_images, train_labels, test_labels)
 
-
-print(train_images.shape)
-print(train_labels.shape)
-print(test_images.shape)
-print(test_labels.shape)
-
-# Dataset
-X , y = spiral_data(samples = 100, classes = 2)
-X_test , y_test = spiral_data(samples = 100, classes = 2)
-
-# Reshape to be a list of lists. Inner lists contain an output
-# either 1 or 0 per each output neuron =
-y.reshape(-1, 1)
-y_test.reshape(-1, 1)
 
 # Creating layers and their activation functions
 model = Model()
-model.add(DenseLayer(2, 64))
+model.add(DenseLayer(784, 64))
 model.add(ActivationReLU())
-model.add(DenseLayer(64, 3))
+model.add(DenseLayer(64, 10))
 model.add(ActivationSoftmax())
 model.set(loss = LossCategoricalCrossEntropy(),
           optimizer = OptimizerSGD(learning_rate = 0.45),
           accuracy = AccuracyCategorical())
 model.finalize()
-model.train(X, y, epochs=7000,
-                  print_every=1000,
-                  validation_data = (X_test, y_test)
+model.train(X_train, train_labels, epochs=100,
+                  print_every=100,
+                  validation_data = (X_test, test_labels)
             )
-
-
