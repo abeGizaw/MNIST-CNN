610ba46298a20a38776e7f8751d8d18483e2d483
abeGizaw <gizawabe3@gmail.com>
2024-04-22 14:30:12 -0400

    lil note

--- RunNetworks.py
+++ RunNetworks.py
@@ -1,5 +1,5 @@
 import subprocess
-
+# Got sick of pressing player over and over lolz
 for i in range(10):
     # subprocess.run(["python", "TrainingFeedForward.py"])
     subprocess.run(["python", "TrainingConvolve.py"])
